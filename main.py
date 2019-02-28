8ac10a91a40f59d1618262ebc010df3754fc6f9d
Eugen Saraci <eugen.saraci@gmail.com>
2019-02-28 20:25:02 +0100

    added iddio

--- main.py
+++ main.py
@@ -4,19 +4,13 @@
 # Version: 1.0
 
 
-# COUPLE
-def merge_vert(photos):
-    V = []
-    bound = len(photos)
-    for i in range(0, bound, 2):
-        if i != bound-1:
-            index_1 = photos[i][0]
-            index_2 = photos[i+1][0]
-            tags    = photos[i][3].union(photos[i+1][3])
-            V.append([(index_1, index_2), "V", len(tags), tags])
+def slideshow2txt(S):
+    f = open("results/{}".format(FILENAME), 'w')
+    f.write("{}\n".format(len(S)))  # python will convert \n to os.linesep
 
-    V = sorted(V, key=lambda photo: photo[2], reverse=True)
-    return V
+    for slide in S:
+        f.write("{}\n".format(slide[0]))
+
 
 # COUPLE
 def merge_vert(photos):
@@ -27,16 +21,23 @@ def merge_vert(photos):
             index_1 = photos[i][0]
             index_2 = photos[i+1][0]
             tags    = photos[i][3].union(photos[i+1][3])
-            V.append([(index_1, index_2), "V", len(tags), tags])
+            V.append(["{} {}".format(index_1, index_2), "V", len(tags), tags])
 
     V = sorted(V, key=lambda photo: photo[2], reverse=True)
     return V
+
+
+# COUPLE2
+def slideshow(H, V):
+    H.extend(V)
+    S = sorted(H, key=lambda photo: photo[2], reverse=True)
+    return S
+
 
 
 def main():
-
     ALBUM = []
-    with open("./datasets/c_memorable_moments.txt") as f:
+    with open("./datasets/{}".format(FILENAME)) as f:
         n_photos = f.readline()
 
         for i, line in enumerate(f.readlines()):
@@ -46,18 +47,22 @@ def main():
             n_tags = int(proc_line[1])
             tags = set(proc_line[2:])
 
-            ALBUM.append([i, orientation, n_tags, tags])
+            ALBUM.append([str(i), orientation, n_tags, tags])
         # print(ALBUM)
 
     V_ALBUM = [photo for photo in ALBUM if photo[1] == "V"]
     V_ALBUM = sorted(V_ALBUM, key=lambda photo: photo[2], reverse=True)
     H_ALBUM = [photo for photo in ALBUM if photo[1] == "H"]
-    H_ALBUM = sorted(H_ALBUM, key=lambda photo: photo[2], reverse=True)
 
-    V_FINAL = merge_vert(V_ALBUM)
-    print(V_FINAL)
+    H_SLIDES = sorted(H_ALBUM, key=lambda photo: photo[2], reverse=True)
+    V_SLIDES = merge_vert(V_ALBUM)
+
+    S = slideshow(H_SLIDES, V_SLIDES)
+    slideshow2txt(S)
 
 
 if __name__ == '__main__':
+
+    FILENAME = "e_shiny_selfies.txt"
     main()
 
