324aa7e663fee7d13563e2176fa2bd3c964522a8
Eugen Saraci <eugen.saraci@gmail.com>
2019-02-28 19:07:36 +0100

    aaa

--- main.py
+++ main.py
@@ -5,7 +5,23 @@
 
 
 def main():
-    print("Hello world")
+
+    album = []
+    with open("./datasets/a_example.txt") as f:
+        n_photos = f.readline()
+
+        for line in f.readlines():
+            proc_line = (line.strip().split(" "))
+
+            orientation = proc_line[0]
+            n_tags = proc_line[1]
+            tags = proc_line[2:]
+
+            album.append([orientation, n_tags, tags])
+        print(album)
+
+
+
 
 
 if __name__ == '__main__':
